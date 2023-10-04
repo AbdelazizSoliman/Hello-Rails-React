4a497314bea9669fd9f617107077ab23ec7d3c8e
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-10-04 16:15:31 +0300

    Fix rubocop errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,8 @@
-class Api::GreetingsController < ApplicationController
-  def random
-    random_greeting = Greeting.order('RANDOM()').first
-    render json: { message: random_greeting.message }
+module Api
+  class GreetingsController < ApplicationController
+    def random
+      random_greeting = Greeting.order('RANDOM()').first
+      render json: { message: random_greeting.message }
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -11,13 +13,13 @@ Greeting.delete_all
 
 # Create new greetings
 greetings = [
-  "Hello, world!",
-  "Welcome to our app.",
-  "Greetings from our team.",
-  "Good morning!",
-  "Have a great day!"
+  'Hello, world!',
+  'Welcome to our app.',
+  'Greetings from our team.',
+  'Good morning!',
+  'Have a great day!'
 ]
 
 greetings.each do |message|
-  Greeting.create(message: message)
+  Greeting.create(message:)
 end
