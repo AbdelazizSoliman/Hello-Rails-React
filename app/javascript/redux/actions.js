c5bc26d09e6d7b074a698ac1150dd26446ebe6f7
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-10-04 15:09:11 +0300

    Create Redux Store, Action, and Reducer

---
+++ app/javascript/redux/actions.js
@@ -0,0 +1,8 @@
+// app/javascript/redux/actions.js
+export const FETCH_GREETING = 'FETCH_GREETING';
+
+export function fetchGreeting() {
+  // Implement your API fetching logic here
+  // You can use libraries like axios or fetch for API calls
+  // Dispatch FETCH_GREETING action with the fetched data
+}
