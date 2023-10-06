b6c40b3307053d80c2a333ddc49430e61c4a24a4
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-10-06 10:49:24 +0300

    Create root controller, update view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'root#index'
   namespace :api do
     get 'greetings/random', to: 'greetings#random'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'api/greetings#random'
 end
